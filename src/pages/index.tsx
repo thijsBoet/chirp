5f2768123054419042c89098384c96d965fda25b
thijsBoet <m.boet@chello.nl>
2023-04-02 14:50:18 +0200

    working posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,136 @@
-import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
-import { type NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { SignIn, SignInButton, SignOutButton, useUser } from '@clerk/nextjs';
+import { type NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
-import { api, } from "~/utils/api";
-import type { RouterOutputs } from "~/utils/api";
+import { api } from '~/utils/api';
+import type { RouterOutputs } from '~/utils/api';
 
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-import { LoadingPage } from "~/components/loading";
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { LoadingPage } from '~/components/loading';
+import { useState } from 'react';
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
-  const { user } = useUser();
+    const { user } = useUser();
 
-  if (!user) return null;
+    const [input, setInput] = useState<string>('')
 
-  return (
-    <div className="flex w-full gap-4">
-      <Image
-        src={user.profileImageUrl}
-        alt={`${user.username} profile picture`}
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
-        priority
-      />
-      <input
-        type="text"
-        placeholder="Type some emojis!"
-        className="w-full grow bg-transparent outline-none"
-      />
-    </div>
-  );
+    const ctx = api.useContext();
+
+    const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+        onSuccess: async () => {
+            setInput('');
+            await ctx.posts.getAll.invalidate();
+        }
+    });
+
+
+	if (!user) return null;
+
+	return (
+		<div className="flex w-full gap-4">
+			<Image
+				src={user.profileImageUrl}
+				alt={`${user.username} profile picture`}
+				className="h-14 w-14 rounded-full"
+				width={56}
+				height={56}
+				priority
+			/>
+			<input
+                type="text"
+                placeholder="Type some emojis!"
+                className="w-full grow bg-transparent outline-none"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                disabled={isPosting}
+            />
+            <button onClick={() => mutate({ content: input })}>Post</button>
+		</div>
+	);
 };
 
-
-type PostWithUser = RouterOutputs["posts"]["getAll"][number]
+type PostWithUser = RouterOutputs['posts']['getAll'][number];
 
 const PostView = (props: PostWithUser) => {
-  const { post, author } = props;
+	const { post, author } = props;
 
-  return (
-    <div
-      key={post.id}
-      className="flex w-full gap-4 border-b border-slate-800 p-4"
-    >
-      <Image
-        src={author.profilePicture}
-        alt={author.username}
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
-      />
-      <div className="flex flex-col">
-        <div className="flex pb-1 text-slate-300">
-          <span className="font-semibold">@{author.username}</span>
-          &nbsp; · &nbsp;
-          <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
-        </div>
-        <span>{post.content}</span>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			key={post.id}
+			className="flex w-full gap-4 border-b border-slate-800 p-4"
+		>
+			<Image
+				src={author.profilePicture}
+				alt={author.username}
+				className="h-14 w-14 rounded-full"
+				width={56}
+				height={56}
+			/>
+			<div className="flex flex-col">
+				<div className="flex text-slate-300">
+					<span className="font-semibold">@{author.username}</span>
+					&nbsp; · &nbsp;
+					<span className="font-thin">
+						{dayjs(post.createdAt).fromNow()}
+					</span>
+				</div>
+				<span className="text-2xl">{post.content}</span>
+			</div>
+		</div>
+	);
 };
 
 const Feed = () => {
-  const { data, isLoading: postLoading } = api.posts.getAll.useQuery();
+	const { data, isLoading: postLoading } = api.posts.getAll.useQuery();
 
-  if (postLoading) return <LoadingPage />
-  if (!data) return <div>Something went wrong</div>;
+	if (postLoading) return <LoadingPage />;
+	if (!data) return <div>Something went wrong</div>;
 
-  return (
-    <div className="flex flex-col">
-            {data?.map((fullPost) => (
-              <PostView {...fullPost} key={fullPost.post.id} />
-            ))}
-    </div>
-  );
-}
+	return (
+		<div className="flex flex-col">
+			{data.map((fullPost) => (
+				<PostView {...fullPost} key={fullPost.post.id} />
+			))}
+		</div>
+	);
+};
 
 const Home: NextPage = () => {
-  const {isLoaded: userLoaded, isSignedIn} = useUser();
+	const { isLoaded: userLoaded, isSignedIn } = useUser();
 
-  // Start loading posts as soon as user is loaded
-  api.posts.getAll.useQuery();
+	// Start loading posts as soon as user is loaded
+	api.posts.getAll.useQuery();
 
-  // Return empoty div if user is not loaded
-  if(!userLoaded) return <div/>
+	// Return empoty div if user is not loaded
+	if (!userLoaded) return <div />;
 
-  return (
-    <>
-      <Head>
-        <title>Chirp - Emoiji Messenger</title>
-        <meta name="description" content="Generated by create-t3-app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="flex h-screen justify-center">
-        <div className="h-full w-full border-x border-x-slate-800 md:max-w-2xl">
-          <div className="p- flex border-b border-slate-800 p-4">
-            {!isSignedIn ? (
-              <div className="flex justify-center">
-                <SignInButton />
-              </div>
-            ) : (
-              <CreatePostWizard />
-            )}
-          </div>
-          <Feed />
-        </div>
-      </main>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Chirp - Emoiji Messenger</title>
+				<meta name="description" content="Generated by create-t3-app" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main className="flex h-screen justify-center">
+				<div className="h-full w-full border-x border-x-slate-800 md:max-w-2xl">
+					<div className="p- flex border-b border-slate-800 p-4">
+						{!isSignedIn ? (
+							<div className="flex justify-center">
+								<SignInButton />
+							</div>
+						) : (
+							<CreatePostWizard />
+						)}
+					</div>
+					<Feed />
+				</div>
+			</main>
+		</>
+	);
 };
 
 export default Home;
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,44 +1,71 @@
-import { User } from "@clerk/nextjs/dist/api";
-import { clerkClient } from "@clerk/nextjs/server";
-import { TRPCError } from "@trpc/server";
-import { z } from "zod";
+import { User } from '@clerk/nextjs/dist/api';
+import { clerkClient } from '@clerk/nextjs/server';
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+	createTRPCRouter,
+	privateProcedure,
+	publicProcedure,
+} from '~/server/api/trpc';
 
 const filterForClient = (user: User) => {
-    return {
-        id: user.id,
-        username: user.username,
-        profilePicture: user.profileImageUrl,
-    }
-}
+	return {
+		id: user.id,
+		username: user.username,
+		profilePicture: user.profileImageUrl,
+	};
+};
 
 export const postsRouter = createTRPCRouter({
-    getAll: publicProcedure.query(async ({ ctx }) => {
-        const posts = await ctx.prisma.post.findMany({
+	getAll: publicProcedure.query(async ({ ctx }) => {
+		const posts = await ctx.prisma.post.findMany({
             take: 100,
-        });
+            orderBy: [{createdAt: 'desc'}],
+		});
 
-        const users = (
-            await clerkClient.users.getUserList({
-                userId: posts.map(post => post.authorId),
-                limit: 100,
-            })
-        ).map(filterForClient)
+		const users = (
+			await clerkClient.users.getUserList({
+				userId: posts.map((post) => post.authorId),
+				limit: 100,
+			})
+		).map(filterForClient);
 
-        return posts.map(post => {
-            const author = users.find(user => user.id === post.authorId);
+		return posts.map((post) => {
+			const author = users.find((user) => user.id === post.authorId);
 
-            if (!author || !author.username) {
-                throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: 'Author for post not found' });
-            }
-            return {
-                post,
-                author: {
-                    ...author,
-                    username: author.username,
-                },
-            }
-        });
-    }),
+			if (!author || !author.username) {
+				throw new TRPCError({
+					code: 'INTERNAL_SERVER_ERROR',
+					message: 'Author for post not found',
+				});
+			}
+			return {
+				post,
+				author: {
+					...author,
+					username: author.username,
+				},
+			};
+		});
+	}),
+
+	create: privateProcedure
+		.input(
+			z.object({
+				content: z.string().emoji("Only emojis are allowed").min(1).max(280),
+			}),
+		)
+		.mutation(async ({ ctx, input }) => {
+			const authorId = ctx.userId;
+
+			const post = await ctx.prisma.post.create({
+				data: {
+                    authorId,
+                    content: input.content,
+				},
+            });
+
+            return post;
+		}),
 });
